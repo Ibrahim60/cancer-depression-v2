2e2dd1cf0bc9c6affcf8b0489ba677446195cfb8
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2026-09-08 01:15:09 +0500

    cleaning

--- auto_data_submitter/reset_tracker.py
+++
@@ -1,7 +0,0 @@
-"""Non-interactive reset of submission state. Requires no stdin."""
-
-from submission_tracker import SubmissionState
-
-with SubmissionState() as state:
-    state.reset()
-print("Tracker reset successfully.")
